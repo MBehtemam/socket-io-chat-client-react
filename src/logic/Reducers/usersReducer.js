19de10a2020d35053e13748eea39eb714bc569f2
MBehtemam <ehtemam@gmail.com>
2019-04-17 20:54:07 +0430

    adding users reducer

--- src/logic/Reducers/usersReducer.js
+++ src/logic/Reducers/usersReducer.js
@@ -1,5 +1,38 @@
-// export const defaultState={};
+import * as Actions from "../Constants/Actions";
+export const defaultState = {};
 
-// const usersReducers = (state=defaultState,action)=>{
-
-// }
+const usersReducers = (state = defaultState, action) => {
+  switch (action.type) {
+    case Actions.USERS_NEW_USER_JOIN: {
+      return {
+        ...state,
+        [action.payload.userId]: {
+          username: action.payload.username,
+          connected: true
+        }
+      };
+    }
+    case Actions.USERS_USER_DISCONNECT: {
+      return {
+        ...state,
+        [action.payload.userId]: {
+          ...state[action.payload.userId],
+          connected: false
+        }
+      };
+    }
+    case Actions.USERS_USER_CHANGE_USERNAME: {
+      return {
+        ...state,
+        [action.payload.userId]: {
+          ...state[action.payload.userId],
+          username: action.payload.username
+        }
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+export default usersReducers;
---
+++ src/logic/Reducers/usersReducer.test.js
@@ -0,0 +1,66 @@
+import usersReducer, { defaultState } from "./usersReducer";
+import * as Actions from "../Constants/Actions";
+
+describe("Test suits fro users reducer", () => {
+  test("to have no user with initializing", () => {
+    expect(usersReducer(undefined, {})).toEqual({});
+  });
+  test("add new user", () => {
+    const userId = "guest-110";
+    const username = "guest-110";
+    expect(
+      usersReducer(undefined, {
+        type: Actions.USERS_NEW_USER_JOIN,
+        payload: { userId, username }
+      })
+    ).toHaveProperty(userId);
+  });
+  test("user disconnected", () => {
+    const userId = "guest-115";
+    const state = {
+      ...defaultState,
+      [userId]: {
+        username: userId,
+        connected: true
+      }
+    };
+    expect(
+      usersReducer(state, {
+        type: Actions.USERS_USER_DISCONNECT,
+        payload: { userId }
+      })
+    ).toEqual({
+      ...state,
+      [userId]: {
+        ...state[userId],
+        connected: false
+      }
+    });
+  });
+  test("user change username", () => {
+    const userId = "guest-110";
+    const state = {
+      ...defaultState,
+      [userId]: {
+        userId,
+        username: "loop"
+      }
+    };
+    const newUserName = "hope";
+    expect(
+      usersReducer(state, {
+        type: Actions.USERS_USER_CHANGE_USERNAME,
+        payload: {
+          userId: "guest-110",
+          username: newUserName
+        }
+      })
+    ).toEqual({
+      ...state,
+      [userId]: {
+        ...state[userId],
+        username: newUserName
+      }
+    });
+  });
+});
